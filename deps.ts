e5e95a2ae780c7e87f74d160afa5df7968aaa343
fastelectronicvegetable <48640397+ckoshka@users.noreply.github.com>
2022-10-21 08:43:58 +1300

    bumped version

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
 export * as R from "https://esm.sh/ramda@0.28.0";
 //export { pipe } from 'https://esm.sh/fp-ts/function';
 //export * as E from "https://esm.sh/fp-ts/Either";
-export * from "https://cdn.jsdelivr.net/gh/ckoshka/shadow_types/valid.ts"; 
+export * from "https://cdn.jsdelivr.net/gh/ckoshka/shadow_types/mod.ts"; 
 export { punctuation } from "https://cdn.jsdelivr.net/gh/ckoshka/unicode_sets/punctuation.ts";  
 export { pack, unpack } from "https://cdn.skypack.dev/msgpackr@v1.7.0-beta1";
 export { cache } from "https://deno.land/x/cache@0.2.13/mod.ts";
